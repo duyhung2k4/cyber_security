3f5174ea3ac0c7b63264dd66dd7fc13ea828da59
ndhung <nguyenduyhung04092004@gmail.com>
2024-09-17 11:19:51 +0700

    .

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { ROUTER_APP } from "@/constant/router";
 import { Route, Routes } from "react-router-dom";
 import { PageAffine, PageCaesarCipher, PageSubstitutionCipher, PageHome } from "./lazy";
+import { LoadingOverlay } from "@mantine/core";
 
 
 
 const RouterApp: React.FC = () => {
     return (
-        <Routes>
-            <Route path={ROUTER_APP.HOME.href} element={<PageHome />} />
-            <Route path={ROUTER_APP.AFFINE.href} element={<PageAffine />} />
-            <Route path={ROUTER_APP.CAESAR_CIPHER.href} element={<PageCaesarCipher />} />
-            <Route path={ROUTER_APP.SUBSTITUTION_CIPHER.href} element={<PageSubstitutionCipher />} />
-        </Routes>
+        <Suspense fallback={<LoadingOverlay visible overlayProps={{ radius: "sm", blur: 2 }} />}>
+            <Routes>
+                <Route path={ROUTER_APP.HOME.href} element={<PageHome />} />
+                <Route path={ROUTER_APP.AFFINE.href} element={<PageAffine />} />
+                <Route path={ROUTER_APP.CAESAR_CIPHER.href} element={<PageCaesarCipher />} />
+                <Route path={ROUTER_APP.SUBSTITUTION_CIPHER.href} element={<PageSubstitutionCipher />} />
+            </Routes>
+        </Suspense>
     )
 }
 
